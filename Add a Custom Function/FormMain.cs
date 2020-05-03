214aaaf442f7113702f7bcdb8e32ff9052c03d84
kenvnus <34490540+kenvnus@users.noreply.github.com>
2020-05-02 22:15:07 -0400

    Issue 12 Literate Programming
    doing literate programming for 2 programs: Edit-Dashboard-In-Designer and Add-a-Custon-Function

--- Add a Custom Function/FormMain.cs
+++ Add a Custom Function/FormMain.cs
@@ -5,6 +5,9 @@ using System;
 using System.Linq;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 
 namespace Add_a_Custom_Function
 {
@@ -14,6 +17,7 @@ namespace Add_a_Custom_Function
         {
             public static decimal MySum(object value)
             {
+                //Debug.Assert(value != null);
                 if (!ListExt.IsList(value))
                     return Stimulsoft.Base.Helpers.StiValueHelper.TryToDecimal(value);
 
@@ -26,7 +30,8 @@ namespace Add_a_Custom_Function
         public FormMain()
         {
             InitializeComponent();
-
+            //dictionary variable to hold the template names and its values (the path of the template file.)
+            //these variables will be binded to datasource of the drop down list cmbTemplates
             var dict = new Dictionary<string, string>
             {
                 {"Select a template",""},
@@ -34,19 +39,20 @@ namespace Add_a_Custom_Function
                 {"Exchange Tenders","Dashboards\\Exchange Tenders.mrt"},
                 {"Fast Food Lunch","Dashboards\\Fast Food Lunch.mrt"}
             };
+            //binding the dict collection to the datasource for the drop down list
             cmbTemplates.DataSource = new BindingSource(dict, null);
             cmbTemplates.DisplayMember = "Key";
             cmbTemplates.ValueMember = "Value";
+            //disable the button "Open dashboard Designer" till users select an item from the drop down list.
             buttonDesigner.Enabled = false;
             AddCustomFunction();
+            // Associate the event-handling method with the SelectedIndexChanged event.
             this.cmbTemplates.SelectedIndexChanged += new System.EventHandler(cmbTemplates_SelectedIndexChanged);
             cmbTemplates.Focus();
-
             // How to Activate
             //Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnO...";
             //Stimulsoft.Base.StiLicense.LoadFromFile("license.key");
             //Stimulsoft.Base.StiLicense.LoadFromStream(stream);
-           
         }
 
         private void AddCustomFunction()
@@ -60,36 +66,47 @@ namespace Add_a_Custom_Function
                 new[] { "values" },
                 new[] { "A set of values" }).UseFullPath = false;
         }
-
+        // This method is called when the user changes his or her selection.
+        // It will enable the button "Open Dashboard Designer" if the selected item is not the first one (prompt "Select an item").
+        // otherwise prompt a user to select a template.
         private void cmbTemplates_SelectedIndexChanged(Object sender, System.EventArgs e) 
         {
-            //Cursor = Cursors.WaitCursor;
-            //Cursor = Cursors.Default;
-            buttonDesigner.Enabled = true;
-        }
-
-        private void buttonDesigner_Click(object sender, EventArgs e)
-        {
-            var report = StiReport.CreateNewDashboard();
-            if (cmbTemplates.Text.Length == 0)
+            if (cmbTemplates.SelectedItem == null || cmbTemplates.SelectedIndex == 0)
             {
-                string key = ((KeyValuePair<String, String>)cmbTemplates.SelectedItem).Key;
-                string value = ((KeyValuePair<String, String>)cmbTemplates.SelectedItem).Value;
-                report.Load(value);
+                // The first item from the the template drop down list is the prompt "Select an item"
+                buttonDesigner.Enabled = false;
+                MessageBox.Show("Please select a template from drop down list.", "Edit Dashboard In Designer-FormMain");
+                cmbTemplates.Focus();
             }
             else
             {
+                //a template is selected, enable the button "Open dashboard Designer"
+                buttonDesigner.Enabled = true;
+            }               
+        }
+        //Function will load the template file into the report object and create a new dashboard.       
+        private void buttonDesigner_Click(object sender, EventArgs e)
+        {
+            //instantiate a new dashboard by using the StiReport library
+            var report = StiReport.CreateNewDashboard();
+            //The first item at position 0 is the prompt "Select an Item"
+            if (cmbTemplates.SelectedIndex != 0)
+            {
+                string key = ((KeyValuePair<String, String>)cmbTemplates.SelectedItem).Key;
+                //Retrieve the path of the template file from the template drop down list 
+                string value = ((KeyValuePair<String, String>)cmbTemplates.SelectedItem).Value;
+                
+                //Debug.Assert(File.Exists(value));
+                //loading a report template from the file (value is the path of the file)
+                report.Load(value);
+            }
+            else //default template is Christmas.mrt
+            {
+                //Debug.Assert(Directory.Exists("Dashboards\\Christmas.mrt"));
                 report.Load("Dashboards\\Christmas.mrt");               
             }
+            // Calls the designer for the report in the Modal window.
             report.Design();
         }
-
-        //private void buttonDesigner_Click(object sender, EventArgs e)
-        //{
-        //    var report = StiReport.CreateNewDashboard();
-        //    report.Load("Dashboards\\Christmas.mrt");
-
-        //    report.Design();
-        //}
     }
 }
--- Edit Dashboard in the Designer/FormMain.cs
+++ Edit Dashboard in the Designer/FormMain.cs
@@ -3,6 +3,9 @@ using System;
 using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 
 namespace Edit_Dashboard_in_the_Designer
 {
@@ -11,7 +14,8 @@ namespace Edit_Dashboard_in_the_Designer
         public FormMain()
         {
             InitializeComponent();
-
+            //dictionary variable to hold the template names and its values (the path of the template file.)
+            //these variables will be binded to datasource of the drop down list cmbTemplates
             var dict = new Dictionary<string, string>
             {
                 {"Select a template",""},
@@ -19,32 +23,31 @@ namespace Edit_Dashboard_in_the_Designer
                 {"Exchange Tenders","Dashboards\\Exchange Tenders.mrt"},
                 {"Fast Food Lunch","Dashboards\\Fast Food Lunch.mrt"}
             };
+            //binding the dict collection to the datasource for the drop down list
             cmbTemplates.DataSource = new BindingSource(dict, null);
             cmbTemplates.DisplayMember = "Key";
             cmbTemplates.ValueMember = "Value";
-        
+            //disable the button "Open dashboard Designer" till users select an item from the drop down list.
+            buttonEdit.Enabled = false;
+            buttonNew.Enabled = false;
+            // Associate the event-handling method with the SelectedIndexChanged event.
+            this.cmbTemplates.SelectedIndexChanged += new System.EventHandler(cmbTemplates_SelectedIndexChanged);
+            cmbTemplates.Focus();
             // How to Activate
             //Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnO...";
             //Stimulsoft.Base.StiLicense.LoadFromFile("license.key");
             //Stimulsoft.Base.StiLicense.LoadFromStream(stream);
         }
 
-        //private StiReport GetTemplate()
-        //{
-        //    var report = new StiReport();
-        //    report.Load("Dashboards\\Christmas.mrt");
-
-        //    return report;
-        //}
-
         private StiReport GetTemplate()
         {
             var report = new StiReport();
-            if (cmbTemplates.SelectedValue != null)
+            if (cmbTemplates.Text.Length != 0)
             {
                 string key = ((KeyValuePair<String,String>)cmbTemplates.SelectedItem).Key;
                 string value = ((KeyValuePair<String, String>)cmbTemplates.SelectedItem).Value;
-                //MessageBox.Show(key + " , " + value);               
+                //Debug.Assert(File.Exists(value));
+                //loading a report template from the file (value is the path of the file)            
                 report.Load(value);                              
             }
             else
@@ -53,39 +56,42 @@ namespace Edit_Dashboard_in_the_Designer
             }
             return report;
         }
-
+        // This method is called when the user changes his or her selection.
+        // It will enable the button "Open Dashboard Designer" if the selected item is not the first one (prompt "Select an item").
+        // otherwise prompt a user to select a template.
         private void cmbTemplates_SelectedIndexChanged(object sender, System.EventArgs e)
         {
-            Cursor = Cursors.WaitCursor;
-            Cursor = Cursors.Default;
-            
-        }
-
-        private void buttonNew_Click(object sender, EventArgs e)
-        {
-            var report = StiReport.CreateNewDashboard();
-            report.Design();
-        }
-
-        private void buttonEdit_Click(object sender, EventArgs e)
-        {
-            if (cmbTemplates.Text.Length == 0)
+            // The first item from the the template drop down list is the prompt "Select an item"
+            if (cmbTemplates.SelectedItem == null || cmbTemplates.SelectedIndex == 0)
             {
+                buttonEdit.Enabled = false;
+                buttonNew.Enabled = false;
                 MessageBox.Show("Please select a template from drop down list.", "Edit Dashboard In Designer-FormMain");
                 cmbTemplates.Focus();
-                return;
+
             }
+            //a template is selected, enable the button "Edit" and button "New"
             else
             {
-                var report = GetTemplate();
-                report.Design();
-            }           
+                buttonEdit.Enabled = true;
+                buttonNew.Enabled = true;
+            }               
         }
-
-        //private void buttonEdit_Click(object sender, EventArgs e)
-        //{          
-        //    var report = GetTemplate();
-        //    report.Design();
-        //}
+        //This method generates a new empty dashboard in a design form 
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            //instantiate a new dashboard by using the StiReport library
+            var report = StiReport.CreateNewDashboard();
+            // Calls the designer for the report in the Modal window.
+            report.Design();
+        }
+        //This method calls the function GetTemplate to load the selected template from drop down list into the design form.
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            //Load the selected template from the drop down list.
+            var report = GetTemplate();
+            //Call the designer for the report in the modal window
+            report.Design();
+        }
     }
 }
